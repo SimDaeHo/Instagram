4d4a9019309305dbb371abc7f05efdc38272de57
daeho sim <tlaeogh5@gmail.com>
2023-05-05 01:59:14 +0900

    로그인 페이지 기능 구현

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,16 @@
+import Signin from "@/components/Signin";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+import { getProviders } from "next-auth/react";
+import { redirect } from "next/navigation";
+
+export default async function SignInpage() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/");
+  }
+
+  const providers = (await getProviders()) ?? {};
+  return <Signin providers={providers} />;
+}
