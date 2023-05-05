d3cfb99a1a932bd553f08452eed7c49526b0be63
daeho sim <tlaeogh5@gmail.com>
2023-05-05 19:42:37 +0900

    로그인 시 이미지 url 표시

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ import NewFillIcon from "./ui/icons/NewFillIcon";
 import { usePathname } from "next/navigation";
 import ColorButton from "./ui/ColorButton";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Avatar from "./Avatar";
 
 const menu = [
   {
@@ -30,6 +31,7 @@ const menu = [
 export default function Navbar() {
   const pathName = usePathname();
   const { data: session } = useSession();
+  const user = session?.user;
   return (
     <div className="flex justify-between items-center px-6">
       <Link href="/">
@@ -42,21 +44,30 @@ export default function Navbar() {
               <Link href={item.href}>{pathName === item.href ? item.clickedIcon : item.icon}</Link>
             </li>
           ))}
-          {session ? (
-            <ColorButton
-              text="Sign out"
-              onClick={() => {
-                signOut();
-              }}
-            />
-          ) : (
-            <ColorButton
-              text="Sign in"
-              onClick={() => {
-                signIn();
-              }}
-            />
+          {user && (
+            <li>
+              <Link href={`/user/${user.username}`}>
+                <Avatar image={user.image} />
+              </Link>
+            </li>
           )}
+          <li>
+            {session ? (
+              <ColorButton
+                text="Sign out"
+                onClick={() => {
+                  signOut();
+                }}
+              />
+            ) : (
+              <ColorButton
+                text="Sign in"
+                onClick={() => {
+                  signIn();
+                }}
+              />
+            )}
+          </li>
         </ul>
       </nav>
     </div>
