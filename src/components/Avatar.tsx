9a791a6002d020d80545b752b7671b06c3ff4068
daeho sim <tlaeogh5@gmail.com>
2023-05-05 20:04:11 +0900

    외부 링크 이미지 x박스 이슈 없애기

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,7 +3,7 @@ type Props = { image?: string | null };
 export default function Avatar({ image }: Props) {
   return (
     <div className="w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.15rem]">
-      <img className="rounded-full p-[0.01rem]" alt="user profile" src={image ?? undefined} />
+      <img className="rounded-full p-[0.01rem]" alt="user profile" src={image ?? undefined} referrerPolicy="no-referrer" />
     </div>
   );
 }
