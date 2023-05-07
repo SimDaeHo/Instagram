c1093e62995e2ba6f4b631c315cf5de9a662e31c
daeho sim <tlaeogh5@gmail.com>
2023-05-07 17:52:01 +0900

    라우트 에러 연결

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import useSWR from "swr";
 export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR("/api/hello");
+  const { data, isLoading, error } = useSWR("/api/me");
   console.log(data);
   return <p>FollowingBar</p>;
 }
