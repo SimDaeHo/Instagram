96c7ede562fb0c69131573991d0e623e31f7f990
daeho sim <tlaeogh5@gmail.com>
2023-05-09 17:05:21 +0900

    포스트 목록 css 적용

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -15,19 +15,19 @@ export default function PostDetail({ post }: Props) {
   const comments = data?.comments;
 
   return (
-    <section>
-      <div className="relative">
-        <Image src={image} alt={`photo by ${username}`} priority fill sizes="650px" />
+    <section className="flex w-full h-full">
+      <div className="relative basis-3/5">
+        <Image className="object-cover" src={image} alt={`photo by ${username}`} priority fill sizes="650px" />
       </div>
-      <div>
+      <div className="w-full basis-2/5 flex flex-col">
         <PostUserAvatar image={userImage} username={username} />
-        <ul>
+        <ul className="border-t border-gray-200 h-full overflow-y-auto p-4 mb-1">
           {comments &&
             comments.map(({ image, username: commentUsername, comment }, index) => (
-              <li key={index}>
+              <li key={index} className="flex items-center mb-1">
                 <Avatar image={image} size="small" highlight={commentUsername === username} />
-                <div>
-                  <span>{commentUsername}</span>
+                <div className="ml-2">
+                  <span className="font-bold mr-1">{commentUsername}</span>
                   <span>{comment}</span>
                 </div>
               </li>
