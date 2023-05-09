1b3c006d713fd535c60377e4bc2382deab86e14f
daeho sim <tlaeogh5@gmail.com>
2023-05-09 20:35:09 +0900

    검색 컴포넌트 css 구현

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -14,14 +14,14 @@ export default function UserSearch() {
     e.preventDefault();
   };
   return (
-    <>
-      <form onSubmit={onSubmit}>
-        <input type="text" autoFocus placeholder="Search for a username or name" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+    <section className="w-full max-w-2xl my-4 flex flex-col items-center">
+      <form className="w-full mb-4" onSubmit={onSubmit}>
+        <input className="w-full text-xl p-3 outline-none border border-gray-400" type="text" autoFocus placeholder="Search for a username or name" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
       </form>
       {error && <p>무언가가 잘못 되었음 😜</p>}
       {isLoading && <GridSpinner />}
       {!isLoading && !error && users?.length === 0 && <p>찾는 사용자가 없음 😭</p>}
-      <ul>
+      <ul className="w-full p-4">
         {users &&
           users.map((user) => (
             <li key={user.username}>
@@ -29,6 +29,6 @@ export default function UserSearch() {
             </li>
           ))}
       </ul>
-    </>
+    </section>
   );
 }
