e0d1feddfff155095512f0c5ddb2d1ff6e27c3ec
daeho sim <tlaeogh5@gmail.com>
2023-05-09 19:48:59 +0900

    검색 데이터 불러오기

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,11 +1,33 @@
 "use client";
 
-import { useState } from "react";
+import { ProfileUser } from "@/model/user";
+import { FormEvent, useState } from "react";
 import useSWR from "swr";
+import GridSpinner from "./ui/GridSpinner";
 
 export default function UserSearch() {
-  const [keyword, setkeyword] = useState("");
-  const { data, isLoading, error } = useSWR(`/api/search/${keyword}`);
-  console.log(data);
-  return <></>;
+  const [keyword, setKeyword] = useState("");
+  const { data: users, isLoading, error } = useSWR<ProfileUser[]>(`/api/search/${keyword}`);
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="text" autoFocus placeholder="Search for a username or name" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+      </form>
+      {error && <p>무언가가 잘못 되었음 😜</p>}
+      {isLoading && <GridSpinner />}
+      {!isLoading && !error && users?.length === 0 && <p>찾는 사용자가 없음 😭</p>}
+      <ul>
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <p>{user.username}</p>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
