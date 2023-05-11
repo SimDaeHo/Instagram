9d96c9a2662a6b9444908302444ff9c076c818dc
daeho sim <tlaeogh5@gmail.com>
2023-05-11 18:31:33 +0900

    좋아요, 북마크, 포스트 아이콘 가져오기

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -2,14 +2,33 @@
 import { ProfileUser } from "@/model/user";
 import { useState } from "react";
 import useSWR from "swr";
+import PostIcon from "./ui/icons/PostIcon";
+import BookmarkIcon from "./ui/icons/BookmarkIcon";
+import HeartIcon from "./ui/icons/HeartIcon";
 
 type Props = {
   user: ProfileUser;
 };
+const tabs = [
+  { type: "posts", icon: <PostIcon /> },
+  { type: "saved", icon: <BookmarkIcon className="w-3 h-3" /> },
+  { type: "liked", icon: <HeartIcon className="w-3 h-3" /> },
+];
 
 export default function UserPosts({ user: { username } }: Props) {
   const [tab, setTab] = useState("saved");
   const { data: posts, isLoading, error } = useSWR(`/api/users/${username}/${tab}`);
   console.log(posts);
-  return <></>;
+  return (
+    <section>
+      <ul>
+        {tabs.map(({ type, icon }) => (
+          <li key={type}>
+            <button>{icon}</button>
+            <span>{type}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
