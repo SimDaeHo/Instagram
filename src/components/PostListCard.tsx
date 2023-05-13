a476106c173d69c6830fab20e76a0df37a225315
daeho sim <tlaeogh5@gmail.com>
2023-05-13 16:41:15 +0900

    코멘트가 몇개 달렸는지 볼수 있게끔 구현

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -16,13 +16,19 @@ type Props = {
   priority?: boolean;
 };
 export default function PostListCard({ post, priority = false }: Props) {
-  const { userImage, username, image, createdAt, likes, text } = post;
+  const { userImage, username, image, comments, text } = post;
   const [openModal, setOpenModal] = useState(false);
   return (
     <article className="rounded-lg shadow-md border border-gray-200">
       <PostUserAvatar image={userImage} username={username} />
       <Image className="w-full object-cover aspect-square" src={image} alt={`photo by ${username}`} width={500} height={500} priority={priority} onClick={() => setOpenModal(true)} />
-      <ActionBar post={post} />
+      <ActionBar post={post}>
+        <p>
+          <span className="font-bold mr-1">{username}</span>
+          {text}
+        </p>
+        {comments > 1 && <button className="font-bold my-2 text-sky-500" onClick={() => setOpenModal(true)}>{`View all ${comments} comments`}</button>}
+      </ActionBar>
       <CommentForm />
       {openModal && (
         <ModalPortal>
