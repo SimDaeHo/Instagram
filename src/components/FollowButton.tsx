c11c9be077eb90c8884eff64efd0527721ef0d91
daeho sim <tlaeogh5@gmail.com>
2023-05-14 21:27:38 +0900

    팔로우 버튼 누르면 숫자 바로 바뀌게 구현

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -2,6 +2,9 @@
 import { ProfileUser } from "@/model/user";
 import Button from "./ui/Button";
 import useMe from "@/hooks/me";
+import { useState, useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { PulseLoader } from "react-spinners";
 
 type Props = {
   user: ProfileUser;
@@ -9,15 +12,36 @@ type Props = {
 export default function FollowButton({ user }: Props) {
   const { username } = user;
   const { user: loggedInUser, toggleFollow } = useMe();
-
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isUpdating = isPending || isFetching;
   const showButton = loggedInUser && loggedInUser.username !== username;
   const following = loggedInUser && loggedInUser.following.find((item) => item.username === username);
 
   const text = following ? "Unfollow" : "Follow";
 
-  const handleFollow = () => {
-    toggleFollow(user.id, !following);
+  const handleFollow = async () => {
+    setIsFetching(true);
+    await toggleFollow(user.id, !following);
+    setIsFetching(false);
+    startTransition(() => {
+      router.refresh();
+    });
   };
 
-  return <>{showButton && <Button text={text} onClick={handleFollow} red={text === "Unfollow"} />}</>;
+  return (
+    <>
+      {showButton && (
+        <div>
+          {isUpdating && (
+            <div>
+              <PulseLoader size={6} />
+            </div>
+          )}
+          <Button text={text} onClick={handleFollow} red={text === "Unfollow"} />
+        </div>
+      )}
+    </>
+  );
 }
