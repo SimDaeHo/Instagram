b409d77d16633776797ac5054de48956b8d9b281
daeho sim <tlaeogh5@gmail.com>
2023-05-15 17:52:23 +0900

    이미지 드래그 되도록 구현

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,20 +1,50 @@
 "use client";
 import { AuthUser } from "@/model/user";
+import { ChangeEvent, DragEvent, useState } from "react";
 import PostUserAvatar from "./PostUserAvatar";
-import FilesIcon from "./ui/icons/FilesIcon";
 import Button from "./ui/Button";
+import FilesIcon from "./ui/icons/FilesIcon";
 
 type Props = {
   user: AuthUser;
 };
-
 export default function NewPost({ user: { username, image } }: Props) {
+  const [dragging, setDragging] = useState(false);
+  const [file, setFile] = useState<File>();
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const files = e.target?.files;
+    if (files && files[0]) {
+      setFile(files[0]);
+      console.log(files[0]);
+    }
+  };
+  const handleDrag = (e: DragEvent) => {
+    if (e.type === "dragenter") {
+      setDragging(true);
+    } else if (e.type === "dragleave") {
+      setDragging(false);
+    }
+  };
+  const handleDragOver = (e: DragEvent) => {
+    e.preventDefault();
+  };
+  const handleDrop = (e: DragEvent) => {
+    e.preventDefault();
+    setDragging(false);
+    const files = e.dataTransfer?.files;
+    if (files && files[0]) {
+      setFile(files[0]);
+      console.log(files[0]);
+    }
+  };
+
   return (
     <section>
       <PostUserAvatar username={username} image={image ?? ""} />
       <form>
-        <input className="hidden" name="input" id="input-upload" type="file" accept="image/*" />
-        <label htmlFor="input-upload">
+        <input className="hidden" name="input" id="input-upload" type="file" accept="image/*" onChange={handleChange} />
+        <label htmlFor="input-upload" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDragOver} onDrop={handleDrop}>
           <FilesIcon />
           <p>Drag and Drop your image here or click</p>
         </label>
